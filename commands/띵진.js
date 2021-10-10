04d118e7fd26fbf8dfa68e617a9b361fa459daac
jw47lee <dlwldnjs803@gmail.com>
2021-10-10 19:59:21 -0400

    added new image for dding + fmagememe

--- commands/띵진.js
+++ commands/띵진.js
@@ -10,6 +10,7 @@ module.exports = {
 
 function random_dding(){
     const items = ["https://cdn.discordapp.com/attachments/806686189603389520/893991463338270720/9k.png",
+    "https://cdn.discordapp.com/attachments/806686189603389520/895755202613887046/238fbc76458d99e0026a2d1cf1c7c9c55cd8e81f85c6f8eff3c51ba0fb294d62.png",
     "https://cdn.discordapp.com/attachments/806686189603389520/893991494464176158/9k.png",
     "https://cdn.discordapp.com/attachments/806686189603389520/894796461705949204/9k.png",
     "https://cdn.discordapp.com/attachments/806686189603389520/894796184496013322/327a5772aaf7ba4e91a3f969d39bc034.png"
