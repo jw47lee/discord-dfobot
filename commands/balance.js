ca0e15091fd841311617bcc841ee4528babd2476
jw47lee <dlwldnjs803@gmail.com>
2021-10-17 00:30:12 -0400

    Update balance.js

--- commands/balance.js
+++ commands/balance.js
@@ -4,13 +4,26 @@ module.exports = {
     description: "simulate balance patch for fun\n",
     execute(client, message, args, Discord){
 
+        if(args[0]){
+            var percentage = random_percentage(21) - 55;
+            while(percentage == 0){
+                percentage = random_percentage(21) - 55;
+            }
+            var is_buff = "buff: "
+            if(percentage < 0){
+                is_buff = "nerf: "
+            }
+            const msg = `**${args[0]}** ${is_buff}${percentage}%`;
+            return message.reply(msg);
+
+        }
 
         const random_selected_char = random_chars();
 
         var msg = "\`Buff\`:\n";
 
         for(var i = 0; i < 5; i++){
-            const percentage = random_percentage();
+            const percentage = random_percentage(6);
             msg += `**${random_selected_char[i]}** +${percentage}%\t`;
             if(i % 2 == 0 && i != 0){
                 msg += "\n";
@@ -20,7 +33,7 @@ module.exports = {
         msg += "\`Nerf\`:\n";
 
         for(var i = 5; i < 8; i++){
-            const percentage = random_percentage();
+            const percentage = random_percentage(5);
             msg += `**${random_selected_char[i]}** -${percentage}%\t`;
         }
         
@@ -30,9 +43,9 @@ module.exports = {
     }
 }
 
-function random_percentage(){
+function random_percentage(max){
     // 5 ~ 30 percentage
-    return (Math.floor(Math.random() * 6) + 1) * 5;
+    return (Math.floor(Math.random() * max) + 1) * 5;
 }
 
 function random_chars(){
